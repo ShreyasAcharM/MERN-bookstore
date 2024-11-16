1793167227e1d46110d10a0622b201dc9ef5a8e8
Shreyas Achar M <shreyasachar007@gmail.com>
2024-11-16 22:25:19 +0530

    added cors netlify access

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const cart = require("./routes/cart")
 const order = require("./routes/order")
 
 const corsOptions = {
-    origin: ['https://golden-cannoli-fa1da8.netlify.app', 'http://localhost:3000'],  // Allow both Netlify and localhost
+    origin: ['https://steady-strudel-2febde.netlify.app', 'http://localhost:3000'],  // Allow both Netlify and localhost
     methods: 'GET, POST, PUT, DELETE',  // Allow these methods for CORS requests
     credentials: true  // If you need to send cookies or credentials
   };
